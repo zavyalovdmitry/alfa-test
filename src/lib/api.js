4f9d8834fe14413f188db7066f20d062b27e1aa1
Dmitry Zavyalov <zavyalovdmitry@mail.ru>
2022-01-27 21:12:40 -0500

    fix: cors proxy

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,3 +1,5 @@
 export const API_KEY = process.env.REACT_APP_API_KEY;
 
 export const DOGS_API = `https://api.thedogapi.com/v1/images/search`;
+
+export const CORS_PROXY = process.env.REACT_APP_CORS_PROXY;
--- src/redux/dogs/dogsActions.js
+++ src/redux/dogs/dogsActions.js
@@ -3,7 +3,7 @@ import {
   FETCH_DOGS_SUCCESS,
   FETCH_DOGS_FAILURE,
 } from './dogsTypes';
-import { DOGS_API, API_KEY } from '../../lib/api';
+import { DOGS_API, API_KEY, CORS_PROXY } from '../../lib/api';
 
 export const fetchDogsRequest = () => ({
   type: FETCH_DOGS_REQUEST,
@@ -22,7 +22,7 @@ export const fetchDogsFailure = (error) => ({
 export const fetchDogs = () => (dispatch) => {
   dispatch(fetchDogsRequest());
 
-  fetch(`${DOGS_API}?limit=50&order=ASC&api_key=${API_KEY}`)
+  fetch(`${CORS_PROXY}/${DOGS_API}?limit=50&order=ASC&api_key=${API_KEY}`)
     .then((response) => response.json())
     .then((data) => {
       const dogs = data;
